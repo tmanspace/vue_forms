36cf85311434ec74f73f7b007460207436095cc5
Tim <serazutdinov.tim@gmail.com>
2021-04-18 16:03:57 +0300

    BaseInput

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>Create an event</h1>
     <form>
-
       <label>Select a category</label>
       <select v-model="event.category">
         <option
@@ -10,74 +9,39 @@
           :value="option"
           :key="option"
           :selected="option === event.category"
-        >{{ option }}</option>
+          >{{ option }}</option
+        >
       </select>
 
       <h3>Name & describe your event</h3>
 
-      <label>Title</label>
-      <input
-        v-model="event.title"
-        type="text"
-        placeholder="Title"
-        class="field"
-      >
+      <base-input label="Title" v-model="event.title" type="text" />
 
-      <label>Description</label>
-      <input
-        v-model="event.description"
-        type="text"
-        placeholder="Description"
-        class="field"
-      />
+      <base-input v-model="event.description" label="Description" type="text" />
 
       <h3>Where is your event?</h3>
 
-      <label>Location</label>
-      <input
-        v-model="event.location"
-        type="text"
-        placeholder="Location"
-        class="field"
-      />
+      <base-input v-model="event.location" label="Location" type="text" />
 
       <h3>Are pets allowed?</h3>
       <div>
-        <input
-            type="radio"
-            v-model="event.pets"
-            :value="1"
-            name="pets"
-          />
+        <input type="radio" v-model="event.pets" :value="1" name="pets" />
         <label>Yes</label>
       </div>
 
       <div>
-        <input
-          type="radio"
-          v-model="event.pets"
-          :value="0"
-          name="pets"
-        />
+        <input type="radio" v-model="event.pets" :value="0" name="pets" />
         <label>No</label>
       </div>
 
       <h3>Extras</h3>
       <div>
-        <input
-          type="checkbox"
-          v-model="event.extras.catering"
-          class="field"
-        />
+        <input type="checkbox" v-model="event.extras.catering" class="field" />
         <label>Catering</label>
       </div>
 
       <div>
-        <input
-          type="checkbox"
-          v-model="event.extras.music"
-          class="field"
-        />
+        <input type="checkbox" v-model="event.extras.music" class="field" />
         <label>Live music</label>
       </div>
 
@@ -87,7 +51,9 @@
 </template>
 
 <script>
+import BaseInput from '../components/BaseInput.vue'
 export default {
+  components: { BaseInput },
   data () {
     return {
       categories: [
